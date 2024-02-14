c7218fdbf08fcc14cbf87ff7f93bf9900ae46246
her_username <crauciuc.costin@yahoo.com>
2024-02-14 17:59:22 +0200

    board search query, canvas layout

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,12 +1,13 @@
 "use client";
-import React from "react";
-import EmptySearch from "./empty-search";
-import EmptyFavorites from "./empty-favorites";
-import EmptyBoards from "./empty-boards";
 
 import { useQuery } from "convex/react";
+
 import { api } from "@/convex/_generated/api";
+
 import BoardCard from "./board-card";
+import EmptySearch from "./empty-search";
+import EmptyBoards from "./empty-boards";
+import EmptyFavorites from "./empty-favorites";
 import { NewBoardButton } from "./new-board-button";
 
 interface BoardListProps {
@@ -18,16 +19,19 @@ interface BoardListProps {
 }
 
 const BoardList = ({ orgId, query }: BoardListProps) => {
-  const data = useQuery(api.boards.get, { orgId });
+  const data = useQuery(api.boards.get, {
+    orgId,
+    ...query,
+  });
 
-  if (data === undefined)
+  if (data === undefined) {
     return (
       <div>
         <h2 className="text-3xl">
           {query.favorites ? "Favorite boards" : "Team boards"}
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
-          <NewBoardButton orgId={orgId} />
+          <NewBoardButton orgId={orgId} disabled />
           <BoardCard.Skeleton />
           <BoardCard.Skeleton />
           <BoardCard.Skeleton />
@@ -35,12 +39,19 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
         </div>
       </div>
     );
+  }
 
-  if (!data?.length && query.search) return <EmptySearch />;
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
 
-  if (!data?.length && query.favorites) return <EmptyFavorites />;
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
 
-  if (!data?.length) return <EmptyBoards />;
+  if (!data?.length) {
+    return <EmptyBoards />;
+  }
 
   return (
     <div>
@@ -66,5 +77,4 @@ const BoardList = ({ orgId, query }: BoardListProps) => {
     </div>
   );
 };
-
 export default BoardList;
